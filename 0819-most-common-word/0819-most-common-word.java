eb0c0f905567fdcca71c822c6a1986d5094f49e5
Lee <hjcloud@me.com>
2024-07-20 13:55:43 +0900

    Time: 11 ms (89.04%), Space: 42.8 MB (67.48%) - LeetHub

--- 0819-most-common-word/0819-most-common-word.java
+++ 0819-most-common-word/0819-most-common-word.java
@@ -1,35 +1,18 @@
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
-        // \\W+ 는 하나 이상의 연속된 비-단어 문자를 뜻하는 정규식으로 쉼표와 공백 문자가 같이 있는 경우, 하나의 공백 문자로 대체한다.
+        // 비교 메서드를 제공하는 Set으로 변경
+        Set<String> ban = new HashSet<>(Arrays.asList(banned));
+        // 각 단어별 개수
+        Map<String, Integer> counts = new HashMap<>();
+        // \\W+ 는 하나 이상의 연속된 비-단어 문자를 뜻하는 정규식으로 쉼표와 공백 문자가 같이 있는 경우, 하나의 공백 문자로 대체
         String[] words = paragraph.replaceAll("\\W+", " ").toLowerCase().split(" ");
-        Map<String, Integer> map = new HashMap<>();
 
         for (String word : words) {
-            boolean isNotBanned = true;
-            for (String banWord : banned) {
-                if (word.equals(banWord)) {
-                    isNotBanned = false;
-                    break;
-                }
-            }
-            if (isNotBanned) {
-                if (map.containsKey(word)) {
-                    map.put(word, map.get(word) + 1);
-                } else {
-                    map.put(word, 1);
-                }
+            if (!ban.contains(word)) {
+                counts.put(word, counts.getOrDefault(word, 0) + 1);
             }
         }
 
-        int max = 0;
-        String mostCommonWord = "";
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > max) {
-                max = entry.getValue();
-                mostCommonWord = entry.getKey();
-            }
-        }
-
-        return mostCommonWord;
+        return Collections.max(counts.entrySet(), Map.Entry.comparingByValue()).getKey();
     }
 }
