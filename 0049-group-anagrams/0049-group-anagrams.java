2fa3cc19c836a46c74fca66ea45ddc52828c4088
Lee <hjcloud@me.com>
2024-07-22 13:47:55 +0900

    Time: 6 ms (97.98%), Space: 48 MB (28.83%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,29 +1,16 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<Integer, String> map = new HashMap<>();
-        List<List<String>> lists = new ArrayList<>();
-        for (int i = 0; i < strs.length; i++) {
-            char[] chars = strs[i].toCharArray();
+        Map<String, List<String>> results = new HashMap<>();
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
             Arrays.sort(chars);
-            map.put(i, String.valueOf(chars));
-        }
-        List<Integer> indexList = map.entrySet().stream()
-                                                .sorted(Map.Entry.comparingByValue())
-                                                .map(Map.Entry::getKey)
-                                                .collect(Collectors.toList());
-        String prev = map.get(indexList.get(0));
-        List<String> list = new ArrayList<>();
-        for (int i : indexList) {
-            if (map.get(i).equals(prev)) {
-                list.add(strs[i]);
-            } else {
-                lists.add(list);
-                list = new ArrayList<>();
-                list.add(strs[i]);
-                prev = map.get(i);
+            String key = String.valueOf(chars);
+
+            if (!results.containsKey(key)) {
+                results.put(key, new ArrayList<>());
             }
+            results.get(key).add(s);
         }
-        lists.add(list);
-        return lists;
+        return new ArrayList<>(results.values());
     }
 }
